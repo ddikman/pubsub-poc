2924167255d77578fc7744ed222af54f1dd43335
David <ddikman@gmail.com>
2020-02-11 21:10:46 +0900

    Fixed ready probe for webhook

--- publisher.rb
+++ publisher.rb
@@ -1,14 +1,26 @@
 require "google/cloud/pubsub"
 require 'sinatra'
 
+set :bind, '0.0.0.0'
+
 TOPIC_NAME = "article-updates-topic"
 
 pubsub = Google::Cloud::Pubsub.new
 
 topic = pubsub.topic TOPIC_NAME
 
-# runs default on port 4567
+get '/' do
+  puts "called root"
+  "index"
+end
+
+get '/healthy' do
+  puts "called healthy!"
+  "healthy!"
+end
+
 get '/:data' do |data|
   topic.publish "Recieved: #{data}"
   puts "Message published: #{data}"
+  "posted message: #{data}"
 end
--- subscriber.rb
+++ subscriber.rb
@@ -4,11 +4,6 @@ SUBSCRIPTION_NAME = "article-updates-sub"
 
 pubsub = Google::Cloud::Pubsub.new
 
-# wait to allow publisher to publish
-sleep 60
-
-puts "Listening.."
-
 subscription = pubsub.subscription SUBSCRIPTION_NAME
 subscriber   = subscription.listen do |received_message|
   puts "Received message: #{received_message.data}"
@@ -16,7 +11,11 @@ subscriber   = subscription.listen do |received_message|
 end
 
 subscriber.start
-# Let the main thread sleep for 60 seconds so the thread for listening
-# messages does not quit
-sleep 60
-subscriber.stop.wait!
+
+# keep the program running, listening for messages
+puts "keeping app alive.."
+while true do
+  sleep 5
+end
+
+puts "exiting.."
